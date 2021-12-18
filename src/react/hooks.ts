cf38e09fe8d3865973f3a2c37fa9886967f7657f
Emmanuel Maro <emmanuelmaro733@gmail.com>
2021-12-18 08:34:21 +0300

    1.0.2

--- src/react/hooks.ts
+++ src/react/hooks.ts
@@ -1,7 +1,36 @@
 import { hookFactory } from "./useHook";
 
+/**
+ * React hook to get client's config methods
+ *
+ * @returns Mzawadie's client's config methods
+ */
+// export const useMzawadieConfig = hookFactory("config");
+
+/**
+ * React hook to get authorization methods
+ *
+ * @returns Mzawadie's authorization methods
+ */
 export const useAuth = hookFactory("auth");
 
+/**
+ * React hook to get user's account methods
+ *
+ * @returns Mzawadie's user's account methods
+ */
+// export const useUser = hookFactory("user");
+
+/**
+ * React hook to get checkout methods
+ *
+ * @returns Mzawadie's checkout methods
+ */
 export const useCheckout = hookFactory("checkout");
 
+/**
+ * React hook to get cart methods
+ *
+ * @returns Mzawadie's cart methods
+ */
 export const useCart = hookFactory("cart");
